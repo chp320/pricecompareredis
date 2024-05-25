2a798b887723ed2efc72648372f71a1132195f70
chp320 <chp320@naver.com>
2024-05-26 01:38:48 +0900

    레디스 sorted set 조회 간단 기능 구현

--- src/main/java/com/example/pricecompareredis/config/RedisConfig.java
+++ src/main/java/com/example/pricecompareredis/config/RedisConfig.java
@@ -4,9 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+/**
+ * Redis 관련 설정
+ * - connection, serialize, ...
+ */
 @Configuration
 @RequiredArgsConstructor
 public class RedisConfig {
@@ -16,5 +23,17 @@ public class RedisConfig {
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
         return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+    }
+
+    // (start) connectionFactory 를 사용할 템플릿 작성
+    @Bean
+    @Primary
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());       // 앞서 만든 connectionFactory 로 접속을 하겠다 !!
+        redisTemplate.setKeySerializer(new StringRedisSerializer());        // key 로 받은 Object 를 직렬화 !!
+        redisTemplate.setValueSerializer(new StringRedisSerializer());      // value 로 받은 Object 를 직렬화 !!
+
+        return redisTemplate;
     }
 }
--- src/main/java/com/example/pricecompareredis/service/LowestPriceServiceImpl.java
+++ src/main/java/com/example/pricecompareredis/service/LowestPriceServiceImpl.java
@@ -1,5 +1,7 @@
 package com.example.pricecompareredis.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
@@ -8,8 +10,13 @@ import java.util.Set;
 @Service
 public class LowestPriceServiceImpl implements LowestPriceService {
 
+    @Autowired
+    private RedisTemplate myProductPriceRedis;
+
     public Set getZsetValue(String key) {
         Set myTempSet = new HashSet();
+        myTempSet = myProductPriceRedis.opsForZSet().rangeWithScores(key, 0, 9);
+
         return myTempSet;
     }
 }
