72bb17afa4b27037b176f885379a5338b045db1d
chp320 <chp320@naver.com>
2024-05-26 23:01:54 +0900

    레디스 sorted set 등록 간단 기능 구현, swagger 통한 테스트

--- src/main/java/com/example/pricecompareredis/controller/LowestPriceController.java
+++ src/main/java/com/example/pricecompareredis/controller/LowestPriceController.java
@@ -1,10 +1,9 @@
 package com.example.pricecompareredis.controller;
 
 import com.example.pricecompareredis.service.LowestPriceService;
+import com.example.pricecompareredis.vo.Product;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
@@ -14,8 +13,14 @@ public class LowestPriceController {
 
     @Autowired
     private LowestPriceService myLowestPriceService;
+
     @GetMapping("/getZSETValue")
     public Set getZsetValue(String key) {
         return myLowestPriceService.getZsetValue(key);
+    }
+
+    @PutMapping("/product")
+    public int setNewProduct(@RequestBody Product newProduct) {
+        return myLowestPriceService.setNewProduct(newProduct);
     }
 }
--- src/main/java/com/example/pricecompareredis/service/LowestPriceServiceImpl.java
+++ src/main/java/com/example/pricecompareredis/service/LowestPriceServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.pricecompareredis.service;
 
+import com.example.pricecompareredis.vo.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -18,5 +19,14 @@ public class LowestPriceServiceImpl implements LowestPriceService {
         myTempSet = myProductPriceRedis.opsForZSet().rangeWithScores(key, 0, 9);
 
         return myTempSet;
+    }
+
+    public int setNewProduct(Product newProduct) {
+        int rank = 0;
+        myProductPriceRedis.opsForZSet().add(newProduct.getProdGrpId(), newProduct.getProductId(), newProduct.getPrice());
+        rank = myProductPriceRedis.opsForZSet().rank(newProduct.getProdGrpId(), newProduct.getProductId()).intValue();
+
+
+        return rank;
     }
 }
